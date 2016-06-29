cdb1d8902e287caea17617806b1881616fa8394e
Parag M <admin@bitsofparag.com>
2016-06-29 11:56:17 +0200

    006a add page action creator

--- app/actions/index.js
+++ app/actions/index.js
@@ -3,15 +3,26 @@
  */
 
 const actionTypes = {
-  ADD_ELEMENT: 'ADD_ELEMENT'
+  ADD_ELEMENT: 'ADD_ELEMENT',
+  ADD_PAGE: 'ADD_PAGE'
 };
 
 let elementId = 0;
+let pageId = 0;
 
 export const addElement = (element) => {
   return {
     type: actionTypes.ADD_ELEMENT,
     id: `element${++elementId}`,
     element
-  }
+  };
+};
+
+
+export const addPage = (page) => {
+  return {
+    type: actionTypes.ADD_PAGE,
+    id: `page${++pageId}`,
+    page
+  };
 };
